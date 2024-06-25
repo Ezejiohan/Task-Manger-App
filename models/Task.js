d9e1a813ac45f253c5b0d1e062219e459a70eb02
Ezejiohan <ezejiohan@gmail.com>
2024-06-25 12:47:16 +0100

    update

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'must provide a name'],
+    trim: true,
+    maxlength: [20, 'name can not be more than 20 characters'],
   },
   completed: {
     type: Boolean,
