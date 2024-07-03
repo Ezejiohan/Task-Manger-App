5249bbe786eb3f92f8ea6adaef61d4c2d2b7dd00
Ezejiohan <ezejiohan@gmail.com>
2024-07-03 20:25:11 +0100

    new update

--- controllers/comment.js
+++ controllers/comment.js
@@ -37,10 +37,40 @@ const editComment = asyncWrapper(async(req, res) => {
 });
 
 const getAllComment = asyncWrapper(async(req,res) => {
-    
+    const {id:taskID} = req.params;
+    const task = await Task.findById({_id:taskID});
+    if (!task) {
+        return next(createCustomError(`No task with id : ${taskID}`, 404)); 
+    }
+    const comments = await Comment.find({});
+    res.status(200).json({comments});
+});
+
+const getComment = asyncWrapper(async(req, res) => {
+    const {id:commentID} = req.params;
+    const comment = await Comment.findOne({_id:commentID});
+    if (!comment) {
+        return next(createCustomError(`no comment with id : ${commentID}`, 404));  
+    }
+    res.status(200).json({comment})
+});
+
+const deleteComment = asyncWrapper(async(req, res) => {
+    const {id:commentID} = req.params;
+    const comment = await Comment.findOneAndDelete({_id:commentID});
+    if (!comment) {
+        return next(createCustomError(`no comment with id : ${commentID}`, 404)); 
+    }
+    const user = req.user;
+    if (comment.user === true) {
+        return next(createCustomError(`You are not authorised to delete comment`, 404)); 
+    }
+    res.status(200).json({comment});
 })
 
 module.exports = { createComment, 
     editComment,
-    getAllComment
+    getAllComment,
+    getComment,
+    deleteComment
 }
--- routes/comments.js
+++ routes/comments.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createComment, editComment } = require('../controllers/comment');
+const { createComment, editComment, getAllComment, getComment, deleteComment } = require('../controllers/comment');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
 const commentRoute = express.Router();
 
 commentRoute.post('/comments', authMiddleware, (createComment));
 commentRoute.patch('/comments/:id', authMiddleware, (editComment));
-
+commentRoute.get('/comments/', authMiddleware, (getAllComment));
+commentRoute.get('/comments/:id', authMiddleware, (getComment));
+commentRoute.delete('/comments/:id', authMiddleware, (deleteComment));
 
 module.exports = {commentRoute}
